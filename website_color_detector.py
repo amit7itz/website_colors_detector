452ae3464cff2d554a50dc0c8e4a8741f600b491
Amit Itzkovitch <amit7itz@gmail.com>
2023-05-01 17:02:53 +0300

    made the script remove similar colors and only keep the most frequent one

--- website_color_detector.py
+++ website_color_detector.py
@@ -6,6 +6,7 @@ import io
 import PIL.PngImagePlugin
 import typing
 import time
+import numpy
 
 def rgb2hex(r,g,b):
     return "#{:02x}{:02x}{:02x}".format(r,g,b)
@@ -16,17 +17,29 @@ def hex2rgb(hexcode):
 def is_vibrant(pixel):
     return (max(pixel) - min(pixel)) > 60
 
+def colors_distance(color1: typing.Tuple[int], color2: typing.Tuple[int]) -> float:
+    p1 = numpy.array(color1)
+    p2 = numpy.array(color2)
+    return numpy.linalg.norm(p1 - p2)
+
+def remove_similar_colors(colors: typing.List[typing.Tuple[int]]):
+    unique_colors = []
+    for color in colors:
+        if all(map(lambda u: colors_distance(u, color) > 20, unique_colors)):
+            unique_colors.append(color)
+    return unique_colors
+
 def get_website_colors_from_image(image: PIL.PngImagePlugin.PngImageFile) -> typing.List[str]:
     pix = image.load()
     width, height = image.size
     c = Counter()
-    for line in range(height):
-        for column in range(width):
-            color = pix[column, line][:3]
-            if is_vibrant(color):
-                c[color] += 1
-    common_colors = c.most_common()[:5]
-    return [color for color, _ in common_colors]
+    for count, rgba in image.getcolors(image.size[0]*image.size[1]):
+        color = rgba[:3]
+        if is_vibrant(color):
+            c[color] = count
+    common_colors = [color for color, _ in c.most_common()[:5]]
+    common_colors = remove_similar_colors(common_colors)
+    return common_colors
 
 def get_image_from_url(url: str) -> PIL.PngImagePlugin.PngImageFile:
     if not url.startswith("http"):
